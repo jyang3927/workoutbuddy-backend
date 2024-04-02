6cd773e24ce78c4975738fbdb6b0d55b664c70f8
Fawaz Butt <fawazbutt@Fawazs-MacBook-Pro-2.local>
2024-04-02 15:41:53 -0400

    added datesController and datesRepository

--- functions/src/routes/datesRouter.ts
+++ functions/src/routes/datesRouter.ts
@@ -1,25 +1,42 @@
-import express from 'express'; 
-import * as datesController from '../controllers/datesController'; 
-import { firebaseAuthMiddleware } from '../middleware/firebaseAuthMiddleware';
+import express from "express";
+import * as datesController from "../controllers/datesController";
+import { firebaseAuthMiddleware } from "../middleware/firebaseAuthMiddleware";
 
-const datesRouter = express.Router(); 
+const datesRouter = express.Router();
 
-//Get all dates for each user 
-datesRouter.get("/users/:userId/dates", firebaseAuthMiddleware, datesController.getAllDates); 
+//Get all dates for each user
+datesRouter.get(
+  "/users/:userId/dates",
+  firebaseAuthMiddleware,
+  datesController.getAllDates
+);
 
 //Get all routines for date
-// datesRouter.get("/users/:userId/dates/:date/routines", firebaseAuthMiddleware, datesController.getAllRountinesforDate); 
+datesRouter.get(
+  "/users/:userId/dates/:date/routines",
+  firebaseAuthMiddleware,
+  datesController.getAllRountinesforDate
+);
 
-// //creating new date 
-// datesRouter.post("/users/:userId/dates/:date", firebaseAuthMiddleware, datesController.addDate); 
+// //creating new date
+datesRouter.post(
+  "/users/:userId/dates/:date",
+  firebaseAuthMiddleware,
+  datesController.addDateToUser
+);
 
-// //edit date 
-// datesRouter.patch("/users/:userId/dates/:date", firebaseAuthMiddleware, datesController.editDate); 
+// //edit date
+datesRouter.patch(
+  "/users/:userId/dates/:date",
+  firebaseAuthMiddleware,
+  datesController.editDate
+);
 
-// //delete date 
-// datesRouter.delete("/users/:userId/dates/:date", firebaseAuthMiddleware, datesController.deleteDate); 
+// //delete date
+datesRouter.delete(
+  "/users/:userId/dates/:date",
+  firebaseAuthMiddleware,
+  datesController.deleteDate
+);
 
-export default datesRouter; 
-
-
-
+export default datesRouter;
