5a74a74491db61ab3efa8db1f1a9a8a1bb1e0247
Fawaz Butt <fawazbutt@Fawazs-MacBook-Pro-2.local>
2024-04-02 15:49:53 -0400

    fixing older code

--- functions/src/routes/usersRouter.ts
+++ functions/src/routes/usersRouter.ts
@@ -8,28 +8,28 @@ const usersRouter = express.Router();
 usersRouter.get("/users/:userId", firebaseAuthMiddleware, usersController.getUser); 
 
 //create user by uId from firebase
-// usersRouter.post("/users", firebaseAuthMiddleware, usersController.addUser); 
-//     //TODO: create an addUser function in usersController & usersRepo (name it differently)
+usersRouter.post("/users", firebaseAuthMiddleware, usersController.addUser); 
+    //TODO: create an addUser function in usersController & usersRepo (name it differently)
 
-// //grab all favorite exercises for user
-// usersRouter.get("/users/:userId/favorites/exercises", firebaseAuthMiddleware, usersController.getFavExercises); 
-//     //TODO: create getFavExercises function in usersController & usersRepo (name differently) 
+//grab all favorite exercises for user
+usersRouter.get("/users/:userId/favorites/exercises", firebaseAuthMiddleware, usersController.getFavExercises); 
+    //TODO: create getFavExercises function in usersController & usersRepo (name differently) 
 
-// //grab all favorite routines for user 
-// usersRouter.get("/users/:userId/favorites/routines", firebaseAuthMiddleware, usersController.getFavRoutines); 
-//     //TODO: create getFavRoutines function in usersController & usersRepo (name differently)
+//grab all favorite routines for user 
+usersRouter.get("/users/:userId/favorites/routines", firebaseAuthMiddleware, usersController.getFavRoutines); 
+    //TODO: create getFavRoutines function in usersController & usersRepo (name differently)
 
-// //add exercise to favorite exercises
-// usersRouter.patch("/users/:userId/favorites/exercises", firebaseAuthMiddleware, usersController.addFavExercise); 
-//     //TODO: create addFavExercise function in usersController & usersRepo (name differently)
+//add exercise to favorite exercises
+usersRouter.patch("/users/:userId/favorites/exercises", firebaseAuthMiddleware, usersController.addFavExercise); 
+    //TODO: create addFavExercise function in usersController & usersRepo (name differently)
 
-// //add routine to favorite routines
-// usersRouter.patch("/users/:userId/favorites/routines", firebaseAuthMiddleware, usersController.addFavRoutine); 
-//     //TODO: create addFavRoutine function in usersController & usersRepo (name differently)
+//add routine to favorite routines
+usersRouter.patch("/users/:userId/favorites/routines", firebaseAuthMiddleware, usersController.addFavRoutine); 
+    //TODO: create addFavRoutine function in usersController & usersRepo (name differently)
 
-// //delete exercise from favorite exercises 
-// usersRouter.delete("/users/:userId/favorites/exercises/:exercise", firebaseAuthMiddleware, usersController.deleteFavExercise); 
+//delete exercise from favorite exercises 
+usersRouter.delete("/users/:userId/favorites/exercises/:exercise", firebaseAuthMiddleware, usersController.deleteFavExercise); 
 
-//delete routine from favorite routines
+delete routine from favorite routines
 
 export default usersRouter; 
