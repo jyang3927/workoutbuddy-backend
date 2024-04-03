606e915767420255b82540535411a0753605c154
Fawaz Butt <fawazbutt@Fawazs-MacBook-Pro-2.local>
2024-04-03 16:59:54 -0400

    added Edit set and Edit Exercise

--- functions/src/models/Date.ts
+++ functions/src/models/Date.ts
@@ -2,9 +2,13 @@ import { ObjectId } from "mongodb";
 import { Routines } from "./Routines";
 
 export interface Date {
-    _id?: ObjectId; 
-    uId: string;
-    date: string; 
-    routines: Routines[]; 
-    workedOut: boolean;
+  _id?: ObjectId;
+  uId: string;
+  date: string;
+  routines: Routines[];
+  workedOut: boolean;
 }
+
+// /dates/:id/routines/exercises/set -- update set (weight, reps etc)
+// /dates/:id/routines/exercises -- update exercise (make something in wireframe)
+// BOTH ARE PUT's DUE TO MULTIPLE ITEMS BEING EDITED INSIDE OBJECTS
--- functions/src/routes/datesRouter.ts
+++ functions/src/routes/datesRouter.ts
@@ -7,32 +7,30 @@ const datesRouter = express.Router();
 //Get all dates for each user
 datesRouter.get("/dates", firebaseAuthMiddleware, datesController.getAllDates);
 
-// Get all routines for date
-// datesRouter.get(
-//   "/dates/:date/routines",
-//   firebaseAuthMiddleware,
-//   datesController.getAllRoutinesforDate
-// );
-
-// //creating new date
+//creating new date
 datesRouter.post(
   "/dates/",
   firebaseAuthMiddleware,
   datesController.addDateToUser
 );
-
-// //edit date
-datesRouter.patch(
-  "/dates/:id",
-  firebaseAuthMiddleware,
-  datesController.editDate
-);
-
-// //delete date
+//delete date
 datesRouter.delete(
   "/dates/:id",
   firebaseAuthMiddleware,
   datesController.deleteDate
 );
 
+//edit specific Set
+datesRouter.put(
+  "/dates/:id/routines/exercises/set",
+  firebaseAuthMiddleware,
+  datesController.editSet
+);
+
+//edit specific exercise
+datesRouter.put(
+  "/dates/:id/routines/exercises",
+  firebaseAuthMiddleware,
+  datesController.editExercises
+);
 export default datesRouter;
